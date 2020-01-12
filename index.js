75549a8a5209bcd112fd0aa6604cb050ac61c7cc
Sarah <sarkwon89@gmail.com>
2020-01-11 20:55:59 -0800

    applied animate.css to welcome section

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+//Materialize functions
 $(document).ready(function () {
     $('.sidenav').sidenav();
 });
+
+
+//animate.css functions
+
+
+$(function(){
+    $('#enter').click(function(){
+        animate('#aboutmeheader', 'fadeInLeft')
+
+    })
+    return false;
+})
+
+
+function animate(element, animation) {
+    $(element).addClass('animated ' + animation);
+    var wait = setTimeout(function() {
+            $(element).removeClass(`animated ` + animation);
+        },
+        1000);
+    }
