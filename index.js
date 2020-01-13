274c27bfad30b114e358aae9a2f3e6e2e63bf3c6
Sarah <sarkwon89@gmail.com>
2020-01-12 23:23:34 -0800

    created carousel

---
+++ index.js
@@ -0,0 +1,11 @@
+//Materialize js
+
+$(document).ready(function () {
+    $('.sidenav').sidenav();
+});
+
+
+$('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+});
