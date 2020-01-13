988798420ce2be0f6471cfebb4135a7655ec0def
Sarah <sarkwon89@gmail.com>
2020-01-13 00:26:40 -0800

    add jquery to apply animated css

--- index.js
+++ index.js
@@ -9,3 +9,13 @@ $('.carousel.carousel-slider').carousel({
     fullWidth: true,
     indicators: true
 });
+
+function animted(element, animation){
+    $(element).addClass('animated ' + animation)
+}
+
+$("#enter").click(function(){
+    animted(".aboutmeheader", "fadeInLeft");
+    animted(".aboutmeheader2", "fadeInRight");
+    animted(".nav-wrapper", "fadeInDown");
+})
